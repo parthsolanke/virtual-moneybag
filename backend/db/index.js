7074f3e8e92aa6e6d27bc88d62ff001caaee6193
Parth Solanke <parthsolanke2002@gmail.com>
2024-01-27 22:50:13 +0530

    db boileroutes

---
+++ backend/db/index.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// connect to db
+mongoose.connect("mongodb+srv://parthsolanke:" + process.env.MONGO_PW + "@cluster0.u7lcir4.mongodb.net/payment-app")
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch(err => {
+        console.log("Error connecting to Database", err);
+    });
+
+// schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+// model
+const User = mongoose.model("User", userSchema);
+
+// export
+module.exports = User;
---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const rootRouter = require('./routes/index');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", rootRouter);
+
+// start server
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log("Error starting server");
+    }
+    console.log("Server running on Port", PORT);
+});
