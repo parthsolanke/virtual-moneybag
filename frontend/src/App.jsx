a1d19a9c1feac64ed44b85874ca9b2142bf2f448
Parth Solanke <parthsolanke2002@gmail.com>
2024-01-30 14:04:21 +0530

    dummy components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Route } from 'react-router-dom';
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Dashboard } from './pages/Dashboard';
+import { Send } from './pages/Send';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/send" element={<Send/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
